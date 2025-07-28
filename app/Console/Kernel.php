81a443f2b4ea9e0d1f3e18d6e919c6ecdbc6e330
yusptar <yusupvatar@gmail.com>
2025-07-28 09:32:32 +0700

    schedule time revision

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,22 +23,25 @@ class Kernel extends ConsoleKernel
   
     protected function schedule(Schedule $schedule)
     {
-        // Schedule Layanan
-        $schedule->command('igd:send')->dailyAt('23:59');
-        $schedule->command('kunjralan:send')->dailyAt('23:59');
-        $schedule->command('bpjsnonbpjs:send')->dailyAt('23:59');
-        $schedule->command('radiologi:send')->dailyAt('23:59');
-        $schedule->command('resep:send')->dailyAt('23:59');
-        $schedule->command('labsampel:send')->dailyAt('23:59');
-        $schedule->command('labparam:send')->dailyAt('23:59');
-        $schedule->command('operasi:send')->dailyAt('23:59');
-        $schedule->command('ralan:send')->dailyAt('23:59');
-        $schedule->command('ranap:send')->dailyAt('23:59');
+        // Schedule Keuangan
 
-        $schedule->command('bor:send')->monthlyOn(1, '00:03'); 
-        $schedule->command('toi:send')->monthlyOn(1, '00:03'); 
-        $schedule->command('alos:send')->monthlyOn(1, '00:03'); 
-        $schedule->command('bto:send')->monthlyOn(1, '00:03'); 
+        // Schedule Layanan
+        $schedule->command('igd:send')->dailyAt('00:03');
+        $schedule->command('kunjralan:send')->dailyAt('00:04');
+        $schedule->command('bpjsnonbpjs:send')->dailyAt('00:05');
+        $schedule->command('radiologi:send')->dailyAt('00:06');
+        $schedule->command('resep:send')->dailyAt('00:07');
+        $schedule->command('labsampel:send')->dailyAt('00:08');
+        $schedule->command('labparam:send')->dailyAt('00:09');
+        $schedule->command('operasi:send')->dailyAt('00:10');
+        $schedule->command('ralan:send')->dailyAt('00:12');
+        $schedule->command('ranap:send')->dailyAt('00:14');
+        $schedule->command('bor:send')->monthlyOn(1, '00:01'); 
+        $schedule->command('toi:send')->monthlyOn(1, '00:01'); 
+        $schedule->command('alos:send')->monthlyOn(1, '00:02'); 
+        $schedule->command('bto:send')->monthlyOn(1, '00:02');
+        
+        // Schedule IKT
         
 
     }
